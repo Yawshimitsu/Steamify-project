343e958d719c3172e77e5e10daee40cb4c35ba45
ViktorTroyanovych <79584138+ViktorTroyanovych@users.noreply.github.com>
2022-11-13 18:46:28 +0000

    fixed cart: only works in the intended pages

--- js/index.js
+++ js/index.js
@@ -1,9 +1,26 @@
-let cartSelector = document.getElementById("cart");
-
 if (localStorage.getItem("cartCounter") === null) {
   localStorage.setItem("cartCounter", 0);
 }
-cartSelector.textContent = localStorage.getItem("cartCounter");
+if (
+  document.URL.includes("index.html") ||
+  document.URL.includes("games.html")
+) {
+  let cartSelector = document.getElementById("cart");
+  cartSelector.textContent = localStorage.getItem("cartCounter");
+
+  // adding shopping cart
+  function addToCart(item) {
+    let cartCounter = parseInt(localStorage.getItem("cartCounter"));
+    cartCounter++;
+    localStorage.setItem("cartCounter", cartCounter);
+    cartSelector.textContent = localStorage.getItem("cartCounter");
+  }
+  // adding empty cart
+  function emptyCart() {
+    localStorage.setItem("cartCounter", 0);
+    cartSelector.textContent = localStorage.getItem("cartCounter");
+  }
+}
 
 if (document.URL.includes("games.html")) {
   document.querySelector(".gaming-section").style.paddingTop = "116px";
@@ -35,21 +52,26 @@ if (document.URL.includes("index.html")) {
           return `
                 <div class="tab_item">
                     <div class="tab_item_cap">
-                        <img class="tab_item_cap_img" src="${item && item.thumb
-            }" />
+                        <img class="tab_item_cap_img" src="${
+                          item && item.thumb
+                        }" />
                     </div>
                     <div class="wrap-content">
                         <div class="tab_item_content">
                             <h4> ${item && item.title}</h4>
                         </div>
                         <div class="pricing">
-                            <h4 class="dealrating"> ${item && item.dealRating
-            }%</h4>
-                            <h4 class="normalPrice"> ${item && item.normalPrice
-            }$</h4>
-                            <button id="${item.internalName
-            }" onclick="addToCart(${item.internalName
-            })">Buy</button>
+                            <h4 class="dealrating"> ${
+                              item && item.dealRating
+                            }%</h4>
+                            <h4 class="normalPrice"> ${
+                              item && item.normalPrice
+                            }$</h4>
+                            <button id="${
+                              item.internalName
+                            }" onclick="addToCart(${
+            item.internalName
+          })">Buy</button>
                         </div>
                     </div>
                 </div>
@@ -64,18 +86,6 @@ if (document.URL.includes("index.html")) {
       console.log("error", error);
     });
 })();
-// adding shopping cart
-function addToCart(item) {
-  let cartCounter = parseInt(localStorage.getItem("cartCounter"));
-  cartCounter++;
-  localStorage.setItem("cartCounter", cartCounter);
-  cartSelector.textContent = localStorage.getItem("cartCounter");
-}
-// adding empty cart
-function emptyCart() {
-  localStorage.setItem("cartCounter", 0);
-  cartSelector.textContent = localStorage.getItem("cartCounter");
-}
 
 // Mobila hamburgarbaren
 const burger = document
@@ -98,7 +108,6 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   });
 });
 
-
 // Cities tabort, lägg till och ändra namn
 (function fetchCities() {
   fetch("https://avancera.app/cities/")
@@ -110,7 +119,7 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     })
     .then((data) => {
       const startSelect = '<select name="cities" id="cities">';
-      const endSelect = '</select>';
+      const endSelect = "</select>";
       const option = data
         .map((item) => {
           return `
@@ -119,9 +128,9 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
         })
         .join("");
       const html = startSelect + option + endSelect;
-      document.querySelectorAll(".listCities").forEach(selector => {
+      document.querySelectorAll(".listCities").forEach((selector) => {
         selector.insertAdjacentHTML("afterbegin", html);
-      })
+      });
     })
     .catch((error) => {
       console.log("error", error);
@@ -129,8 +138,9 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 })();
 
 function renameCity() {
-
-  const cityOldId = document.querySelector('#renameCity .listCities option:checked').value;
+  const cityOldId = document.querySelector(
+    "#renameCity .listCities option:checked"
+  ).value;
   const cityNewName = document.querySelector("#renameCityInput").value;
 
   if (!cityOldId || !cityNewName) {
@@ -138,22 +148,24 @@ function renameCity() {
   }
 
   var patchData = {
-    name: cityNewName
+    name: cityNewName,
   };
 
   fetch("https://avancera.app/cities/" + cityOldId, {
-    method: 'PATCH',
+    method: "PATCH",
     headers: {
       Accept: "application/json, text/plain, /",
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(patchData)
+    body: JSON.stringify(patchData),
   })
-    .then(_ => { location.reload() })
+    .then((_) => {
+      location.reload();
+    })
     .catch((error) => {
       console.log("error", error);
     });
-};
+}
 
 function addCity() {
   const newCityName = document.querySelector("#addCityInput").value;
@@ -161,73 +173,81 @@ function addCity() {
     return;
   }
   var postData = {
-    name: newCityName, population: Math.floor(Math.random() * 100000)
+    name: newCityName,
+    population: Math.floor(Math.random() * 100000),
   };
   fetch("https://avancera.app/cities/", {
-    method: 'post',
+    method: "post",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(postData)
+    body: JSON.stringify(postData),
   })
-    .then(_ => { location.reload() })
+    .then((_) => {
+      location.reload();
+    })
     .catch((error) => {
       console.log("error", error);
     });
-};
+}
 
 function removeCity() {
-  const deleteCityId = document.querySelector('#deleteCity .listCities option:checked').value;
+  const deleteCityId = document.querySelector(
+    "#deleteCity .listCities option:checked"
+  ).value;
 
   if (!deleteCityId) {
     return;
   }
 
   fetch("https://avancera.app/cities/" + deleteCityId, {
-    method: 'delete',
+    method: "delete",
   })
-    .then(_ => { location.reload() })
+    .then((_) => {
+      location.reload();
+    })
     .catch((error) => {
       console.log("error", error);
     });
-};
-
+}
 
 function createChart(games) {
-  const element = document.getElementById('priceChart');
+  const element = document.getElementById("priceChart");
   if (!element) {
     return;
   }
-  const ctx = element.getContext('2d');
-  var prices = games.map(t => t.normalPrice);
-  var amountOfGamesPerPrice = []
+  const ctx = element.getContext("2d");
+  var prices = games.map((t) => t.normalPrice);
+  var amountOfGamesPerPrice = [];
 
-  prices.sort((a, b) => a.localeCompare(b, undefined, { numeric: true })).forEach(p => {
-    amountOfGamesPerPrice[p] = amountOfGamesPerPrice[p] ? amountOfGamesPerPrice[p] + 1 : 1;
-  })
+  prices
+    .sort((a, b) => a.localeCompare(b, undefined, { numeric: true }))
+    .forEach((p) => {
+      amountOfGamesPerPrice[p] = amountOfGamesPerPrice[p]
+        ? amountOfGamesPerPrice[p] + 1
+        : 1;
+    });
 
   const _ = new Chart(ctx, {
-    type: 'bar',
+    type: "bar",
     data: {
       labels: Object.keys(amountOfGamesPerPrice),
-      datasets: [{
-        label: 'Games per price',
-        data: Object.values(amountOfGamesPerPrice),
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)'
-        ],
-        borderWidth: 2
-      }]
+      datasets: [
+        {
+          label: "Games per price",
+          data: Object.values(amountOfGamesPerPrice),
+          backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+          borderColor: ["rgba(255, 99, 132, 1)"],
+          borderWidth: 2,
+        },
+      ],
     },
     options: {
       scales: {
         y: {
-          beginAtZero: true
-        }
-      }
-    }
+          beginAtZero: true,
+        },
+      },
+    },
   });
 }
